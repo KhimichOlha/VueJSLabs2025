26608258bc60e2eefbd5524b80960bda8e7e80ef
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-10-06 20:49:59 +0300

    feate(dashboard): відображення адмін панелі

---
+++ lab2/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterView } from 'vue-router';
+const counter = ref(0);
+</script>
+
+<template>
+  <div>
+    <router-link to="/admin/dashboard/overview">Overview</router-link>
+    <router-link to="/admin/dashboard/activity">Activity</router-link>
+    <button @click="counter++">Збільшити: {{ counter }}</button>
+    <KeepAlive include="Overview,Activity" max="2">
+      <RouterView />
+    </KeepAlive>
+  </div>
+</template>
