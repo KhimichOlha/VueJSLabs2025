e03a2243b336fd687311e720dd892eb0a31642c5
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-10-06 20:55:03 +0300

    feater(Users): відображення для користовучів

---
+++ lab2/src/views/Users.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+import UserItem from '@/components/UserItem.vue';
+
+const users = ref([{ id: 1, name: 'User1' }, { id: 2, name: 'User2' }]);
+const handleEdit = (id) => console.log('Edit user', id);
+</script>
+
+<template>
+  <div>
+    <h2>Users</h2>
+    <UserItem v-for="user in users" :key="user.id" :user="user" @edit="handleEdit" />
+  </div>
+</template>
