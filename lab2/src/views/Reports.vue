4ac6773c93536c4fad8f8d087111db3b28e77788
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-10-06 20:41:18 +0300

    feate(reports): відображення звітів

---
+++ lab2/src/views/Reports.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const reports = ref([{ id: 1, title: 'Звіт 1' }, { id: 2, title: 'Звіт 2' }]);
+</script>
+
+<template>
+  <table>
+    <tr v-for="item in reports" :key="item.id">
+      <slot name="row" :item="item">
+        <td>{{ item.title }}</td> 
+      </slot>
+    </tr>
+  </table>
+</template>
