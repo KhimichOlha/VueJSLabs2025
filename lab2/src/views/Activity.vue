20373e4165dc00bea112c249e43878ef6316ab5b
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-10-06 20:53:30 +0300

    feater(Activity): відображення активності

---
+++ lab2/src/views/Activity.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue';
+const localCounter = ref(0);
+</script>
+
+<template>
+  <div>Activity <button @click="localCounter++">Лічильник: {{ localCounter }}</button></div>
+</template>
