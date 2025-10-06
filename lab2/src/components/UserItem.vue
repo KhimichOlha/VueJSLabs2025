c205065034a0cbbae725422ef29a9ad71b2c38cc
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-10-06 21:05:02 +0300

    feate(UserItem): додано елемент користовеча

---
+++ lab2/src/components/UserItem.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps({ user: Object });
+const emit = defineEmits(['edit']);
+</script>
+
+<template>
+  <div>{{ user.name }} <button @click="emit('edit', user.id)">Edit</button></div>
+</template>
