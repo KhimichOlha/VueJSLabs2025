98140a3c82b3a2d59f1dc7283007e8d0250a6c11
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-10-06 20:51:55 +0300

    feate(Overview): відображення огляду

---
+++ lab2/src/views/Overview.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue';
+const localCounter = ref(0);
+</script>
+
+<template>
+  <div>Overview <button @click="localCounter++">Лічильник: {{ localCounter }}</button></div>
+</template>
