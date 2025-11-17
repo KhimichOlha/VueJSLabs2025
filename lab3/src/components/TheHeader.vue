4eb683bf20b6ce3d57df0800b61e21b3ef25a038
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-17 20:03:17 +0200

    feate(TheHeader.vue): додано навігація та перимиканя теми

---
+++ lab3/src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <div class="logo">VueNews</div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <button @click="$emit('toggle-theme')" class="theme-btn">
+      {{ isDark ? ' Light' : ' Dark' }}
+    </button>
+  </header>
+</template>
+
+<script setup>
+defineProps(['isDark']);
+defineEmits(['toggle-theme']);
+</script>
+
+<style scoped>
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: var(--header-bg);
+  border-bottom: 1px solid var(--border-color);
+}
+
+.logo {
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+
+nav {
+  display: flex;
+  gap: 1rem;
+}
+
+
+.active-link {
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+.theme-btn {
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  border: 1px solid var(--border-color);
+  background: var(--card-bg);
+  color: var(--text-color);
+  border-radius: 4px;
+}
+</style>
