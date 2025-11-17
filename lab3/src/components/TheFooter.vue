18e7cd35b8e42ff5403673d02d035e9891c32241
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-17 20:05:19 +0200

    feate(TheFooter.vue): додано футор

---
+++ lab3/src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <p>©2025 VueLabs. Всі права захищені. Olha Khimich</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 1rem;
+  margin-top: auto; 
+  border-top: 1px solid var(--border-color);
+  font-size: 0.9rem;
+  opacity: 0.8;
+}
+</style>
