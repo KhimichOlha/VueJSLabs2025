6c2faf51a844675aaa7caf9e92c08d1bbaf29ee6
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-21 20:44:53 +0200

    refactor(App.vue): оновлення головної сторінки

--- lab4/src/App.vue
+++ lab4/src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useSettingsStore } from '@/stores/settings';
+
+const settings = useSettingsStore();
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <nav>
+      <router-link to="/">{{ $t('nav.home') }}</router-link> |
+      <router-link to="/products">{{ $t('nav.products') }}</router-link> |
+      <router-link to="/cart">{{ $t('nav.cart') }}</router-link> |
+      <router-link to="/profile">{{ $t('nav.profile') }}</router-link>
+    </nav>
+
+    <div class="lang-switch">
+      <button 
+        @click="settings.setLocale('ua')" 
+        :class="{ active: settings.locale === 'ua' }"
+      >UA</button>
+      <button 
+        @click="settings.setLocale('en')" 
+        :class="{ active: settings.locale === 'en' }"
+      >EN</button>
+    </div>
+  </header>
+
+  <main>
+    <router-view />
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+header { display: flex; justify-content: space-between; padding: 1rem; background: #f4f4f4; margin-bottom: 20px;}
+.active { font-weight: bold; text-decoration: underline; }
+button { margin-left: 5px; cursor: pointer; }
 </style>
