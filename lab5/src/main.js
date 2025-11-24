19b1159867d19a1d192eeaefc63848531967314c
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-24 19:41:05 +0200

    refactor( main.js): підключення прайм компонентів

--- lab5/src/main.js
+++ lab5/src/main.js
@@ -2,4 +2,20 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'; 
+import 'primeicons/primeicons.css'; 
+
+const app = createApp(App);
+
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.my-app-dark', 
+        }
+    }
+});
+
+app.mount('#app');
