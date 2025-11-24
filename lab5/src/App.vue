ced610c629be9f39f996ee2006a6170a6a757edf
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-24 20:10:53 +0200

    refactor(App.vue): підключення контактної форми

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -2,6 +2,7 @@
 import MyHeader from './components/MyHeader.vue';
 import HeroSection from './components/HeroSection.vue';
 import Pricing from './components/Pricing.vue';
+import ContactForm from './components/ContactForm.vue'; 
 import Toast from 'primevue/toast';
 </script>
 
@@ -11,9 +12,11 @@ import Toast from 'primevue/toast';
     <MyHeader />
     <main>
       <HeroSection />
-      
       <Pricing />
       
+      <ContactForm />
+      
     </main>
-  </div>
+    
+    </div>
 </template>
