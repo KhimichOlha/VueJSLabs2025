534cebb73714aa33c1061d68f655178fcea6bfed
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-24 19:48:30 +0200

    refactor(App.vue):підключення компоненту

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HeroSection.vue'
+import MyHeader from './components/MyHeader.vue';
+import HeroSection from './components/HeroSection.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <MyHeader />
+    <main>
+      <HeroSection />
+      
+      </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+
+.app-container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 1rem;
+    font-family: var(--font-family);
 }
 </style>
