ed01efb111a14b05b244dbe985424ea11ad1a51f
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-24 19:57:20 +0200

    refactor(App.vue): підключення компоненту Toast

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
 import MyHeader from './components/MyHeader.vue';
 import HeroSection from './components/HeroSection.vue';
+import Toast from 'primevue/toast'; 
 </script>
 
 <template>
   <div class="app-container">
+    <Toast /> 
+    
     <MyHeader />
     <main>
       <HeroSection />
-      
-      </main>
+    </main>
   </div>
 </template>
 
 <style>
-
 .app-container {
     max-width: 1200px;
     margin: 0 auto;
