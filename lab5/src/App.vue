3a9c0a6f713270dd102684e03d38f8dc0ab85a92
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-24 20:07:33 +0200

    refactor(App.vue): підключення компонентів терифів

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -1,25 +1,19 @@
 <script setup>
 import MyHeader from './components/MyHeader.vue';
 import HeroSection from './components/HeroSection.vue';
-import Toast from 'primevue/toast'; 
+import Pricing from './components/Pricing.vue';
+import Toast from 'primevue/toast';
 </script>
 
 <template>
   <div class="app-container">
-    <Toast /> 
-    
+    <Toast />
     <MyHeader />
     <main>
       <HeroSection />
+      
+      <Pricing />
+      
     </main>
   </div>
 </template>
-
-<style>
-.app-container {
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 0 1rem;
-    font-family: var(--font-family);
-}
-</style>
