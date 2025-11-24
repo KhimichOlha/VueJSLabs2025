c6194a9a4f9adb1774cc56c302ff76d6a7b09ad4
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-24 19:54:25 +0200

    refactor(main.js): додвання CopyToClipboardPlugin

--- lab5/src/main.js
+++ lab5/src/main.js
@@ -3,19 +3,23 @@ import './style.css'
 import App from './App.vue'
 
 import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura'; 
-import 'primeicons/primeicons.css'; 
+import Aura from '@primeuix/themes/aura';
+import 'primeicons/primeicons.css';
+
+import ToastService from 'primevue/toastservice';
+
+
+import CopyToClipboardPlugin from './plugins/copyToClipboard';
 
 const app = createApp(App);
 
-
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
-        options: {
-            darkModeSelector: '.my-app-dark', 
-        }
+        preset: Aura
     }
 });
+
+app.use(ToastService); 
+app.use(CopyToClipboardPlugin); 
 
 app.mount('#app');
