8975997c237d50aea360ea471bacec2b4a229965
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-28 20:53:11 +0200

    feate(index.vue): додано головну сторінку

---
+++ lab6/app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2 class="text-3xl font-bold mb-4">Вітаємо на курсі Nuxt 4!</h2>
+    <p class="text-lg text-gray-600 mb-6">Це лабораторна робота №6, де ми вивчаємо SSR, API та Nuxt UI.</p>
+    <UButton to="/blog" color="primary" size="lg">Читати блог</UButton>
+  </div>
+</template>
