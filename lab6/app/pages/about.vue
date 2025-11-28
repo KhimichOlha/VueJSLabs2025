9640f983566d10c25a8a0b64317f7bfabaaa1799
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-28 20:54:18 +0200

    feate(about.vue): сторінка про проект

---
+++ lab6/app/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold mb-4">Про цей проєкт</h2>
+    <UCard>
+      <p>Цей сайт створено в рамках навчання. Ми використовуємо:</p>
+      <ul class="list-disc list-inside mt-2 ml-4">
+        <li>Nuxt 4 (Future Mode)</li>
+        <li>Nuxt UI</li>
+        <li>Server API (Nitro)</li>
+      </ul>
+    </UCard>
+  </div>
+</template>
