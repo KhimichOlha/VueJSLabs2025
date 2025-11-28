180e2a1b0328833cd062847f855ed9300f80cf2b
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-28 20:57:28 +0200

    feate([slug].vue): диномічна сторінка для відображення запису

---
+++ lab6/app/pages/blog/[slug].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute()
+
+</script>
+
+<template>
+  <div>
+    <UButton to="/blog" variant="link" icon="i-heroicons-arrow-left" class="mb-4">
+      Назад до списку
+    </UButton>
+    
+    <h2 class="text-2xl font-bold">Стаття: {{ route.params.slug }}</h2>
+    <p class="mt-4">Тут буде контент статті...</p>
+  </div>
+</template>
