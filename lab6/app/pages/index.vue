e5e0cb9d81e95f5479e4aeea2f5a44f913bd3732
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2025-11-30 12:12:06 +0200

    refactor(index.vue): додано сео теги

--- lab6/app/pages/index.vue
+++ lab6/app/pages/index.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Головна | Nuxt Course Blog',
+  description: 'Лабораторна робота №6: Вивчення Nuxt 4 та Nuxt UI.',
+  ogTitle: 'Головна | Nuxt Course Blog',
+  ogDescription: 'Лабораторна робота №6: Вивчення Nuxt 4 та Nuxt UI.'
+})
+</script>
+
 <template>
   <div>
     <h2 class="text-3xl font-bold mb-4">Вітаємо на курсі Nuxt 4!</h2>
